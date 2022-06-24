668a9e77c50cae20435eba946abbc2165d16f856
Yuto Mizutani <tenajima@gmail.com>
2022-06-24 09:14:13 +0000

    Get changed files, considering working directory.

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ git fetch --prune --unshallow --no-tags
 
 SQL_FILE_PATTERN="${FILE_PATTERN:?}"
 SOURCE_REFERENCE="origin/${GITHUB_PULL_REQUEST_BASE_REF:?}"
-changed_files=$(git diff --name-only --no-color "$SOURCE_REFERENCE" "HEAD" -- "${SQLFLUFF_PATHS:?}" |
+changed_files=$(git diff --name-only --no-color "$SOURCE_REFERENCE" "HEAD" -- "$INPUT_WORKING_DIRECTORY/${SQLFLUFF_PATHS:?}" |
   grep -e "${SQL_FILE_PATTERN:?}" |
   xargs -I% bash -c 'if [[ -f "%" ]] ; then echo "%"; fi' || :)
 echo "Changed files:"
